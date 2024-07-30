303172ea05dec3fbd139bf72f08ab72594f78a5d
Sadžida Valjevac <valjevac.sadzida02@gmail.com>
2024-07-30 22:28:16 +0200

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/page/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'myBrown': '#B46F36',
+        'chocolate': '#45270F',
+        'myBeige': '#E6C0A1',
+      },    
     },
+    fontFamily: {
+      lexend: ['Lexend Deca'],
+      koulen: ['Koulen',]
+    },
+    
   },
   plugins: [],
-};
+}
